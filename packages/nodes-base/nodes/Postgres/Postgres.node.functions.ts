73da27ddad66bc6b0a10a07472e4e2052d7db971
Alex Grozav <alexgrozav@Alexs-MacBook-Pro.local>
2022-04-08 16:00:53 +0300

    :bug: Added copying json files to gulp task.

--- packages/nodes-base/nodes/Postgres/Postgres.node.functions.ts
+++ packages/nodes-base/nodes/Postgres/Postgres.node.functions.ts
@@ -1,6 +1,6 @@
 import { IDataObject, INodeExecutionData } from 'n8n-workflow';
 import pgPromise from 'pg-promise';
-import pg from 'pg-promise/typescript/pg-subset';
+import * as pg from 'pg-promise/typescript/pg-subset';
 
 /**
  * Returns of a shallow copy of the items which only contains the json data and
--- packages/nodes-base/scripts/build.mjs
+++ packages/nodes-base/scripts/build.mjs
@@ -1,23 +1,38 @@
 import path from 'path';
+import shell from 'shelljs';
 import glob from 'tiny-glob';
 import esbuild from 'esbuild';
 import {fileURLToPath} from 'url';
 
+/**
+ * Configuration
+ */
+
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const rootDir = path.resolve(__dirname, '..');
+const distDir = path.resolve(rootDir, 'dist');
+
+/**
+ * Clean up
+ */
+
+shell.rm('-rf', distDir);
+
+/**
+ * Build
+ */
 
 [
 	'credentials',
 	'nodes',
 	'src'
 ].forEach(async (dir) => {
-	const srcDir = path.resolve(__dirname, '..', dir);
-	const distDir = path.resolve(__dirname, '..', 'dist', dir);
-	const tsFiles = await glob(path.resolve(srcDir, '**/*.ts'));
+	const tsFiles = await glob(path.resolve(rootDir, dir, '**/*!(.d).ts'));
 
 	esbuild.build({
 		entryPoints: tsFiles,
 		format: 'cjs',
-		outdir: distDir,
+		outdir: path.resolve(distDir, dir),
 	}).catch(() => process.exit(1));
 });
