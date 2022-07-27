789dbc86df71abad660cf8aa287f69ca44e94650
paulguilloux-opencell <paul.guilloux@opencellsoft.com>
2022-07-27 14:41:39 +0200

    Fix oAuth2

--- packages/nodes-base/nodes/Opencell/GenericFunctions.ts
+++ packages/nodes-base/nodes/Opencell/GenericFunctions.ts
@@ -14,42 +14,43 @@ import {
 	IHttpRequestMethods,
 	IHttpRequestOptions,
 	IWebhookFunctions,
+	NodeApiError
 } from 'n8n-workflow';
 
 export async function opencellApi(this: IHookFunctions | IWebhookFunctions | IExecuteFunctions | IExecuteSingleFunctions | ILoadOptionsFunctions, method: IHttpRequestMethods, endpoint: string, body: any = {}, query: IDataObject = {}, uri?: string): Promise<any> { // tslint:disable-line:no-any
-	//let authenticationMethod = this.getNodeParameter('authentication', 0);
+
 	const returnData: IDataObject[] = [];
-	const httpBasicAuth = await this.getCredentials('opencellApi');
-	const requestOptions: IHttpRequestOptions = {
-		headers: {
-			'Accept': 'application/json',
-		},
-		url: '',
-		encoding: 'json',
+
+	let requestOptions: OptionsWithUri = {
+		method,
+		headers: {},
+		uri : '',
+		body,
 		json: true,
 	};
-
-	if (httpBasicAuth !== undefined) {
-		requestOptions.auth = {
-			username: httpBasicAuth.username as string,
-			password: httpBasicAuth.password as string,
-		};
-		requestOptions.url = `${httpBasicAuth.host}:${httpBasicAuth.port}`;
-	}
-
-	if (endpoint !== '') {
-		requestOptions.url += endpoint;
-	}
-	requestOptions.method = method;
-	//if (body && body.length > 0) {
-	requestOptions.body = body;
-	//}
+	
 	if (query) {
 		requestOptions.qs = query;
 	}
+
 	try {
-		const responseData = await this.helpers.httpRequest(requestOptions);
-		//console.log(responseData);
+		const authenticationMethod = this.getNodeParameter('authentication', 0);
+		let responseData: any;
+
+		if(authenticationMethod === 'basicAuth') {
+			const httpBasicAuth = await this.getCredentials('opencellApi');
+			requestOptions.auth = {
+				username: httpBasicAuth.username as string,
+				password: httpBasicAuth.password as string,
+			};
+			requestOptions.uri = `${httpBasicAuth.host}:${httpBasicAuth.port}${endpoint}`;
+			responseData = await this.helpers.request!(requestOptions);
+		}
+		else if (authenticationMethod === 'oAuth2') {
+			const credentials = await this.getCredentials('opencellOAuth2Api');
+			requestOptions.uri = `${credentials.host}:${credentials.port}${endpoint}`;
+			responseData = await this.helpers.requestOAuth2!.call(this, 'opencellOAuth2Api', requestOptions, { tokenType: 'Bearer', includeCredentialsOnRefreshOnBody: true });
+		}
 
 		// To return an array when it is a generic API get list
 		if ('POST' === method && responseData['data'] && responseData['data'].length != null) {
@@ -58,10 +59,9 @@ export async function opencellApi(this: IHookFunctions | IWebhookFunctions | IEx
 		}
 		return responseData;
 	} catch (error) {
-		//console.log(error);
 		if (error.response) {
 			const errorMessage = error.response.data.message || error.response.data.errorCode || error.response.data.status;
-			throw new Error(`Opencell error response [${error.response.status}]: ${errorMessage}`);
+			throw new NodeApiError(this.getNode(), {error : `Opencell error response [${error.response.status}]: ${errorMessage}`});
 		}
 		throw error;
 	}
